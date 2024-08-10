21ad44f7fce570f9b8ae123fdf7e47aa7fb4d7b3
michellechan0 <150152668+michellechan0@users.noreply.github.com>
2024-08-10 12:27:23 +0700

    Add files via upload

---
+++ Meet11/flask_app/templates/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+dataI = ["I", "G", "S"]
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/igs')
+def igs():
+    return render_template('igs.html', data=dataI)
+
+if __name__ == '__main__':
+    app.run(debug=True)
